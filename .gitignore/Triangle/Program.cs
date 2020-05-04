cba6c1846c7f03f7a0575e66c0ac75fee9346df1
Westley889 <64759006+Westley889@users.noreply.github.com>
2020-05-03 21:08:39 -0400

    Initial commit

---
+++ .gitignore/Triangle/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1 = Convert.ToDouble(args[0]);
+            double num2 = Convert.ToDouble(args[1]);
+            double num3 = Convert.ToDouble(args[2]);
+
+            int counter = 0;
+            string type = "default";
+            string angle = "default";
+
+            Triangle triangle = new Triangle(num1, num2, num3);
+
+            if (triangle.is_it_triangle() == true)
+            {
+                if ((Math.Max(Math.Max(num1, num2), num3)) == num3)
+                {
+                    angle = triangle.type_of_angle();
+                }
+                else if ((Math.Max(Math.Max(num1, num2), num3)) == num2)
+                {
+                    angle = triangle.type_of_angle();
+                }
+                else
+                {
+                    angle = triangle.type_of_angle();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid triangle");
+                counter++;
+            }
+            if (counter == 0)
+            {
+                type = triangle.side_sizes();
+                Console.WriteLine($"This is an {angle} {type} triangle");
+            }
+        }
+
+        
+    }
+}
---
+++ .gitignore/Triangle/triangle.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Triangle
+{
+    class Triangle
+    {
+        private double n1;
+        private double n2;
+        private double n3;
+
+
+        public Triangle(double num1, double num2, double num3)
+        {
+            n1 = num1; 
+            n2 = num2;
+            n3 = num3;
+        }
+        public bool is_it_triangle()
+        {
+          bool hum = (n1 + n2) > n3 && (n1 + n3) > n2 && (n2 + n3) > n1;
+            return hum;
+        }
+
+        public string type_of_angle()
+        {
+            string ans;
+
+            if (Math.Pow(n1, 2) + Math.Pow(n2, 2) == Math.Pow(n3, 2))
+            {
+                ans = "Right";
+            }
+            else if (Math.Pow(n1, 2) + Math.Pow(n2, 2) < Math.Pow(n3, 2))
+            {
+                ans = "obtuse";
+            }
+            else
+            {
+                ans = "acute";
+            }
+            return ans;
+        }
+        public string side_sizes()
+        {
+            string side;
+            if (n1 == n2 && n3 == n2)
+            {
+                side = "equilateral";
+            }
+            else if (n1 != n2 && n1 != n3 && n2 != n3)
+            {
+                side = "scalene";
+            }
+            else
+            {
+                side = "isoceles";
+            }
+            return side;
+        }
+
+    }
+}
